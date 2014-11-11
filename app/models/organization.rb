393857980a2fbafde5a65479b1e8ac9ef4ae250c
David Rivers <dmr41.rivers@gmail.com>
2014-11-10 21:38:11 -0700

    added overaged, knames and palindrome validation to Perosn and Organization models

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,13 @@
 class Organization < ActiveRecord::Base
 
-  validates :name, presence: true
+  validate :revoname
+
+  def revoname
+    if name.blank?
+      errors.add(:name, "Name can't be blank")
+    elsif name.downcase == name.downcase.reverse
+      errors.add(:name, "Palidrome business names aren't allowed!")
+    end
+  end
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,30 @@
 class Person < ActiveRecord::Base
 
- validates :first_name, presence: true
+ validate :firsty
  validates :last_name, presence: true
- validates :date_of_birth, presence: true
+ validate :aged
+
+   def firsty
+     if first_name.present? && first_name[0] == "K" || first_name[0] == "k"
+       errors.add(:first_name, "begins with K")
+     elsif first_name.blank?
+       errors.add(:first_name, "Name can't be blank")
+     end
+   end
+
+  def aged
+      now = Time.now.utc.to_date
+      if date_of_birth.present?
+        oldy = now.year - date_of_birth.year
+      else
+        oldy = 0
+      end
+    if date_of_birth.blank?
+      errors.add(:date_of_birth, "Date of birth can't be blank")
+    elsif date_of_birth.present? && oldy >= 125
+      errors.add(:date_of_birth, "Age is more than 125 years?  Please try again")
+    end
+  end
+
+
 end
